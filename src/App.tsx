1c164633ec449e01ce6582e26ac525a65abd553a
anton-oz <antonosland789@gmail.com>
2025-06-09 04:39:13 -0500

    refactor: removed all old components, removed css, removed framer-motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import Projects from "./components/Projects";
-import Header from "./components/Header";
 export default function App() {
   return (
     <main id="snapContainer" className="scroll-smooth">
-      <Header />
-      <Projects />
+      <h1>Anton</h1>
     </main>
   );
 }
