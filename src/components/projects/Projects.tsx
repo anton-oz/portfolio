45a8385500c38c382768c1ec3be4874a33411497
anton-oz <antonosland789@gmail.com>
2025-06-21 22:13:55 -0500

    feat: added projects component

---
+++ src/components/projects/Projects.tsx
@@ -0,0 +1,10 @@
+import "./projects.css";
+
+export default function Projects() {
+  return (
+    <section className="project-container">
+      {" "}
+      <h2>Projects</h2>{" "}
+    </section>
+  );
+}
