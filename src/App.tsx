7f28560677d931f74f1527bfd5b229697c677666
anton-oz <antonosland789@gmail.com>
2025-06-23 15:52:06 -0500

    feat: added footer, added project card styling, changed font to DM Sans

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ export default function App() {
         <Splash />
         <Projects />
       </main>
+      <footer>footer stuff</footer>
     </>
   );
 }
--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,10 +1,25 @@
 import "./projects.css";
 
 export default function Projects() {
+  const projects = [
+    { name: "Project1", img: "" },
+    { name: "Project2", img: "" },
+    { name: "Project3", img: "" },
+    { name: "Project4", img: "" },
+    { name: "Project5", img: "" },
+    { name: "Project6", img: "" },
+  ];
   return (
-    <section id="projects" className="project-container">
+    <section id="projects" className="project-section">
       {" "}
       <h2>Projects</h2>{" "}
+      <div className="project-container">
+        {projects.map(({ name, img }, i) => (
+          <div className="project" style={{ background: img }} key={i}>
+            <h3>{name}</h3>
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
